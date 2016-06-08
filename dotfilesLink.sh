486442d30067458c41c22ed1190b41d014672d69
Ryohei Nishiyama <ryohei_nishiyama@forcia.com>
2016-06-08 13:49:59 +0900

    add shell script

---
+++ dotfilesLink.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+ln -s /data/git-repos/dotfiles/.vimrc ~/.vimrc
+ln -s /data/git-repos/dotfiles/.zshrc ~/.zshrc
+ln -s /data/git-repos/dotfiles/.vim ~/.vim
+ln -s /data/git-repos/dotfiles/.tmux.conf ~/.tmux.conf
