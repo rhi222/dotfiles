a5a8084586bd780de50a1a115dcc68d2210dbf92
rhi222 <ryouhei222@gmail.com>
2016-06-10 16:28:38 +0900

    modify for deploy psqlrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s /data/git-repos/dotfiles/.vimrc ~/.vimrc
 ln -s /data/git-repos/dotfiles/.zshrc ~/.zshrc
 ln -s /data/git-repos/dotfiles/.vim ~/.vim
 ln -s /data/git-repos/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s /data/git-repos/dotfiles/.psqlrc /home/postgres/.psqlrc
