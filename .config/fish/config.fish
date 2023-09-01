a2d9d665ff33954c1abec95405a6334943931e1c
rhi222 <ryouhei222@gmail.com>
2023-09-01 11:03:22 +0900

    add: path関連の設定変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,12 +34,16 @@ set PSQL_EDITOR 'nvim'
 # set PATH /home/forcia/.nvm/versions/node/v5.0.0/bin /home/forcia/bin /usr/local/java/bin /usr/local/java/bin /home/forcia/bin /usr/local/sbin /usr/local/bin /usr/local/pgsql/bin /home/forcia/.rbenv/bin /usr/local/python/bin /usr/local/python/bin /home/forcia/.rbenv/shims /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /home/forcia/.fzf/bin /home/forcia/anaconda3/bin $PATH
 
 # set GOROOT /usr/local/go
-# set GOPATH $HOME/go
 # set PATH $PATH $GOROOT/bin
 
 # https://tech.librastudio.co.jp/entry/index.php/2018/02/20/post-1792/
 set GOPATH $HOME/go
 set PATH $PATH $GOPATH/bin
+
+# for deno
+set DENO_INSTALL $HOME/.deno
+set PATH $DENO_INSTALL/bin:$PATH
+
 
 # set -x PATH $HOME/.pyenv/bin $PATH
 # eval (pyenv init - | source)
@@ -47,8 +51,8 @@ set PATH $PATH $GOPATH/bin
 
 # volta setting
 # https://docs.volta.sh/guide/getting-started
-set VOLTA_HOME $HOME/.volta
-set PATH $PATH $VOLTA_HOME/bin
+# http://gitlab.fdev/webconnect/material/material_registration/-/merge_requests/6511
+set VOLTA_FEATURE_PNPM 1
 
 # for win32yank
 # https://qiita.com/v2okimochi/items/f53edcf79a4b71f519b1#%E3%83%9E%E3%82%A6%E3%82%B9%E6%93%8D%E4%BD%9C%E3%82%84%E3%82%AF%E3%83%AA%E3%83%83%E3%83%97%E3%83%9C%E3%83%BC%E3%83%89%E5%85%B1%E6%9C%89%E3%82%92%E8%A8%AD%E5%AE%9A%E3%81%99%E3%82%8B
@@ -98,3 +102,9 @@ set __fish_git_prompt_char_upstream_behind '-'
 #     set_color normal
 # end
 # ------------- }}}
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
