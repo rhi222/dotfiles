be6cdc03fcb17281a3ed26c8b23699a85160ae08
rhi222 <ryouhei222@gmail.com>
2023-09-03 18:34:38 +0900

    update setting

---
+++ .config/nvim/lua/config/formatter.lua
@@ -0,0 +1,19 @@
+-- https://github.com/mhartington/formatter.nvim
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
+
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+      -- "formatter.filetypes.any" defines default configurations for any
+      -- filetype
+      require("formatter.filetypes.any").remove_trailing_whitespace
+    }
+  }
+}
