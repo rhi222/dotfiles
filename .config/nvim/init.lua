afe3aa7582d9582eed0e81d6df9b20fcb0810dc9
rhi222 <ryouhei222@gmail.com>
2023-09-03 17:29:45 +0900

    clean: indent

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,24 +63,24 @@ require('lazy_nvim')
 -- https://zenn.dev/fukakusa_kadoma/articles/99e8f3ab855a56
 local on_attach = function(client, bufnr)
 
-  -- LSPが持つフォーマット機能を無効化する
-  -- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
-  --client.server_capabilities.documentFormattingProvider = false
-  
-  -- 下記ではデフォルトのキーバインドを設定しています
-  -- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
+	-- LSPが持つフォーマット機能を無効化する
+	-- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
+ --client.server_capabilities.documentFormattingProvider = false
+ 
+	-- 下記ではデフォルトのキーバインドを設定しています
+	-- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
 
-  local set = vim.keymap.set
-  set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-  set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-  set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-  set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+	local set = vim.keymap.set
+	set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+	 set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+	 set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+	 set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+	 set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+	 set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+	 set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+	 set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+	 set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+	 set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
 
 end
 
