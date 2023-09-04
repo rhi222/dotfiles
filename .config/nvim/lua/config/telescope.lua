9a146055ff283f5368a3f5b6684d1d599b2afb72
rhi222 <ryouhei222@gmail.com>
2023-09-04 23:36:11 +0900

    add: telescopeでgrep

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,7 +1,7 @@
 -- https://github.com/nvim-telescope/telescope.nvim#usage
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 --
