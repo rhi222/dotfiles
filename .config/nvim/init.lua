62f7f3b3a9590cb40e425116187af5739ff11915
rhi222 <ryouhei222@gmail.com>
2023-09-05 13:41:53 +0900

    add: 構文解析対象を整備

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,7 +71,6 @@ local on_attach = function(client, bufnr)
 	-- LSPが持つフォーマット機能を無効化する
 	-- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
  --client.server_capabilities.documentFormattingProvider = false
- 
 	-- 下記ではデフォルトのキーバインドを設定しています
 	-- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
 
