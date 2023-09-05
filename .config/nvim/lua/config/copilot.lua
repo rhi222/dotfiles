843b0708de2f36e95962c5043ecd28ecf20415c3
rhi222 <ryouhei222@gmail.com>
2023-09-05 10:18:05 +0900

    add: copilotを動かす

--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -1,2 +1,23 @@
 -- https://github.com/zbirenbaum/copilot.lua
-require('copilot').setup()
+require('copilot').setup({
+	enabled = true,
+	filetypes = {
+		javascript = true, -- allow specific filetype
+		typescript = true, -- allow specific filetype
+		markdown = true,
+		sh = function ()
+			if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+				-- disable for .env files
+				return false
+			end
+			return true
+		end,	
+		["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
+	},
+	suggestion = {
+		auto_trigger = true,
+		keymap = {
+			accept = '<tab>',
+		},
+	},
+})
