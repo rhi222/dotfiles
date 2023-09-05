65115b8ddd22d73b2baf9679592ab6a4020d7af8
rhi222 <ryouhei222@gmail.com>
2023-09-05 09:37:45 +0900

    add: telescope use fzf

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -10,6 +10,13 @@ local actions = require "telescope.actions"
 local fb_actions = require "telescope".extensions.file_browser.actions
 require("telescope").setup {
   extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    },
     file_browser = {
       -- theme = "ivy",
       -- disables netrw and use telescope-file-browser in its place
@@ -27,6 +34,8 @@ require("telescope").setup {
     },
   },
 }
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
 -- https://github.com/nvim-telescope/telescope-file-browser.nvim
 require('telescope').load_extension 'file_browser'
 -- open file_browser with the path of the current buffer
