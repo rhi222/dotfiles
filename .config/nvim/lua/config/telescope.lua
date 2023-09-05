a553819c81d6e42a81d03b8913667568ceb5f1e9
rhi222 <ryouhei222@gmail.com>
2023-09-05 15:39:26 +0900

    clean: 一括format

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,56 +1,56 @@
 -- https://github.com/nvim-telescope/telescope.nvim#usage
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- https://github.com/nvim-telescope/telescope-file-browser.nvim/issues/103
-local actions = require "telescope.actions"
-local fb_actions = require "telescope".extensions.file_browser.actions
-require("telescope").setup {
-  defaults = {
-    mappings = {
-      n = {
-        ["q"] = actions.close,
-      },
-    }
-  },
-  extensions = {
-    -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    },
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-          ["h"] = fb_actions.goto_parent_dir,
-          ["l"] = actions.select_default,
-          ["q"] = actions.close,
-        },
-      },
-    },
-  },
-}
+local actions = require("telescope.actions")
+local fb_actions = require("telescope").extensions.file_browser.actions
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			n = {
+				["q"] = actions.close,
+			},
+		},
+	},
+	extensions = {
+		-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
+		file_browser = {
+			theme = "ivy",
+			-- disables netrw and use telescope-file-browser in its place
+			hijack_netrw = true,
+			mappings = {
+				["i"] = {
+					-- your custom insert mode mappings
+				},
+				["n"] = {
+					-- your custom normal mode mappings
+					["h"] = fb_actions.goto_parent_dir,
+					["l"] = actions.select_default,
+					["q"] = actions.close,
+				},
+			},
+		},
+	},
+})
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require("telescope").load_extension, "fzf")
 
 -- https://github.com/nvim-telescope/telescope-file-browser.nvim
-require('telescope').load_extension 'file_browser'
+require("telescope").load_extension("file_browser")
 -- open file_browser with the path of the current buffer
 vim.api.nvim_set_keymap(
-  'n',
-  '<space>f',
-  ':Telescope file_browser path=%:p:h initial_mode=normal select_buffer=true hidden=true<CR>',
-  { noremap = true }
+	"n",
+	"<space>f",
+	":Telescope file_browser path=%:p:h initial_mode=normal select_buffer=true hidden=true<CR>",
+	{ noremap = true }
 )
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,54 +1,54 @@
 return {
 	{
-		't9md/vim-quickhl',
+		"t9md/vim-quickhl",
 		config = function()
-			require('config/vim-quickhl')
-		end
+			require("config/vim-quickhl")
+		end,
 	},
 	-- Detect tabstop and shiftwidth automatically
-	'tpope/vim-sleuth',
+	"tpope/vim-sleuth",
 	{
-		'nvim-lualine/lualine.nvim',
+		"nvim-lualine/lualine.nvim",
 		config = function()
-			require('config/lualine')
-		end
+			require("config/lualine")
+		end,
 	},
 	{
-		'lewis6991/gitsigns.nvim',
+		"lewis6991/gitsigns.nvim",
 		config = function()
-			require('config/gitsigns')
-		end
+			require("config/gitsigns")
+		end,
 	},
 	{
-		'phaazon/hop.nvim',
+		"phaazon/hop.nvim",
 		config = function()
-			require('config/hop')
-		end
+			require("config/hop")
+		end,
 	},
 	{
-		'nvim-treesitter/nvim-treesitter',
+		"nvim-treesitter/nvim-treesitter",
 		config = function()
-			require('config/nvim-treesitter')
-		end
+			require("config/nvim-treesitter")
+		end,
 	},
 	-- colorschemaは検討中
 	{
-		'rebelot/kanagawa.nvim',
+		"rebelot/kanagawa.nvim",
 		-- config = function()
 		-- 	require('config/kanagawa')
 		-- end
 	},
 	-- 'rktjmp/lush.nvim',
 	{
-		'folke/tokyonight.nvim',
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require('config/tokyonight')
-		end
+			require("config/tokyonight")
+		end,
 	},
 	{
-		'catppuccin/nvim',
+		"catppuccin/nvim",
 	},
 	-- copilot.lua使ってみたいが、keymapがうまく出来ずに保留
 	-- {
@@ -57,96 +57,96 @@ return {
 	-- 		require('config/copilot')
 	-- 	end
 	-- },
-	'github/copilot.vim',
+	"github/copilot.vim",
 	{
-		'lukas-reineke/indent-blankline.nvim',
+		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			require('config/indent-blankline')
-		end
+			require("config/indent-blankline")
+		end,
 	},
 	-- LSP/補完
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		dependencies = {
 			-- Useful status updates for LSP
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-			 -- Additional lua configuration, makes nvim stuff amazing!
-			'folke/neodev.nvim',
-    	},
+			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+			-- Additional lua configuration, makes nvim stuff amazing!
+			"folke/neodev.nvim",
+		},
 	},
 	{
-		'williamboman/mason.nvim',
+		"williamboman/mason.nvim",
 		dependencies = {
-			'williamboman/mason-lspconfig'
+			"williamboman/mason-lspconfig",
 		},
 		-- 遅延ロードを試してみる
 		-- https://zenn.dev/yuucu/articles/lazy_nvim_tuning#%E9%81%85%E5%BB%B6%E3%83%AD%E3%83%BC%E3%83%89%E8%A8%AD%E5%AE%9A%E3%81%AE%E7%B4%B9%E4%BB%8B%E3%81%A8%E5%AE%9F%E4%BE%8B
 		cmd = {
-		  "Mason",
-		  "MasonInstall",
-		  "MasonUninstall",
-		  "MasonUninstallAll",
-		  "MasonLog",
-		  "MasonUpdate",
+			"Mason",
+			"MasonInstall",
+			"MasonUninstall",
+			"MasonUninstallAll",
+			"MasonLog",
+			"MasonUpdate",
 		},
 		-- config = function()
 		-- 	require('config/mason')
 		-- end
 	},
 	{
-		'hrsh7th/nvim-cmp',
+		"hrsh7th/nvim-cmp",
 		dependencies = {
 			-- TODO: 要精査
-			'hrsh7th/cmp-nvim-lsp', --LSPを補完ソースに
-			'hrsh7th/cmp-buffer', --bufferを補完ソースに
-			'hrsh7th/cmp-cmdline', -- vimのコマンド
-			'hrsh7th/cmp-path', --pathを補完ソースに
-			'hrsh7th/vim-vsnip', --スニペットエンジン
+			"hrsh7th/cmp-nvim-lsp", --LSPを補完ソースに
+			"hrsh7th/cmp-buffer", --bufferを補完ソースに
+			"hrsh7th/cmp-cmdline", -- vimのコマンド
+			"hrsh7th/cmp-path", --pathを補完ソースに
+			"hrsh7th/vim-vsnip", --スニペットエンジン
 			-- 'hrsh7th/cmp-vsnip', --スニペットを補完ソースに
-			'onsails/lspkind.nvim' --補完欄にアイコンを表示
+			"onsails/lspkind.nvim", --補完欄にアイコンを表示
 		},
 		config = function()
-			require('config/nvim-cmp')
-		end
+			require("config/nvim-cmp")
+		end,
 	},
 	{
-		'mhartington/formatter.nvim',
+		"mhartington/formatter.nvim",
 		config = function()
-			require('config/formatter')
-		end
+			require("config/formatter")
+		end,
 	},
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.2',
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
 		dependencies = {
-			'nvim-lua/plenary.nvim',
+			"nvim-lua/plenary.nvim",
 			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
 			-- Only load if `make` is available. Make sure you have the system
 			-- requirements installed.
 			{
-			  'nvim-telescope/telescope-fzf-native.nvim',
-			  -- NOTE:
-			  -- If you are having trouble with this installation,
-			  -- refer to the README for telescope-fzf-native for more instructions.
-			  build = 'make',
-			  cond = function()
-			    return vim.fn.executable 'make' == 1
-			  end,
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- NOTE:
+				-- If you are having trouble with this installation,
+				-- refer to the README for telescope-fzf-native for more instructions.
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
 			},
 		},
 		config = function()
-			require('config/telescope')
-		end
+			require("config/telescope")
+		end,
 	},
 	{
-		'nvim-telescope/telescope-file-browser.nvim',
+		"nvim-telescope/telescope-file-browser.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	},
 	-- Useful plugin to show you pending keybinds.
 	-- https://github.com/folke/which-key.nvim
 	{
-		'folke/which-key.nvim',
+		"folke/which-key.nvim",
 		event = "VeryLazy",
 		init = function()
 			vim.o.timeout = true
