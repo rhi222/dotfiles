e8d26d1cc2fc1136fdf67065b408fdcc714d65ce
rhi222 <ryouhei222@gmail.com>
2023-09-05 13:41:53 +0900

    add: 構文解析対象を整備

--- .config/nvim/lua/config/nvim-treesitter.lua
+++ .config/nvim/lua/config/nvim-treesitter.lua
@@ -1,14 +1,21 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 require'nvim-treesitter.configs'.setup{
 	ensure_installed = {
+		'bash',
+		'fish',
+		'graphql',
 		'javascript',
 		'lua',
+		'markdown',
 		'python',
+		'sql',
+		'tsv',
 		'tsx',
 		'typescript',
 		'vim',
 		'vimdoc',
-		'xml'
+		'xml',
+		'yaml',
 	},
 	highlight = {
 		enable = true
