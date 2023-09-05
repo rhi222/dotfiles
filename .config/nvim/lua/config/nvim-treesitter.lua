692c2d2467ee4534f584b0b93ce4cd0dfb1ebed7
rhi222 <ryouhei222@gmail.com>
2023-09-05 09:36:15 +0900

    update plugin settings

--- .config/nvim/lua/config/nvim-treesitter.lua
+++ .config/nvim/lua/config/nvim-treesitter.lua
@@ -1,5 +1,15 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 require'nvim-treesitter.configs'.setup{
+	ensure_installed = {
+		'javascript',
+		'lua',
+		'python',
+		'tsx',
+		'typescript',
+		'vim',
+		'vimdoc',
+		'xml'
+	},
 	highlight = {
 		enable = true
 	}
