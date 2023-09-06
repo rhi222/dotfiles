43261ac3c8d2649098a75093c67e04108d68267e
rhi222 <ryouhei222@gmail.com>
2023-09-06 23:35:16 +0900

    clean: comment

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -129,9 +129,6 @@ return {
 		tag = "0.1.2",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-			-- Only load if `make` is available. Make sure you have the system
-			-- requirements installed.
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
 				-- NOTE:
