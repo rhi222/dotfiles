4d16f1dc021ae42cad68142ccaf23cb9a461c3f5
rhi222 <ryouhei222@gmail.com>
2023-10-01 13:05:04 +0900

    add: Migrate to version 3
    https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3
    https://github.com/lukas-reineke/indent-blankline.nvim

--- .config/nvim/lua/config/indent-blankline.lua
+++ .config/nvim/lua/config/indent-blankline.lua
@@ -1,11 +1,6 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
--- vim.opt.list = true
--- vim.opt.listchars:append "tab:▸ "
--- ノイズ気味なので非表示にしたいかも
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol:↴"
 
-require("indent_blankline").setup({
+require("ibl").setup({
 	char = "|",
 	-- 改行コードは好みじゃないので表示しない
 	-- show_end_of_line = true,
