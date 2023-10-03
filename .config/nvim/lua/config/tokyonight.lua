658864ee10f7ffd96afa6a6eebda5186321e349e
rhi222 <ryouhei222@gmail.com>
2023-10-03 12:23:07 +0900

    clean: 必須property

--- .config/nvim/lua/config/tokyonight.lua
+++ .config/nvim/lua/config/tokyonight.lua
@@ -1,6 +1,8 @@
 -- https://github.com/folke/tokyonight.nvim
 require("tokyonight").setup({
 	style = "moon",
+	on_colors = function(_) end,
+	on_highlights = function(_, _) end,
 })
 
 vim.cmd([[colorscheme tokyonight]])
