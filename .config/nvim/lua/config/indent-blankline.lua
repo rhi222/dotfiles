0d13b4acb38fd191e2e001646f60a30e4c1c4007
rhi222 <ryouhei222@gmail.com>
2024-03-20 20:50:24 +0900

    clean: indent-blanklineの設定整理

--- .config/nvim/lua/config/indent-blankline.lua
+++ .config/nvim/lua/config/indent-blankline.lua
@@ -1,10 +1,8 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
 require("ibl").setup({
-	indent = { char = "|" },
-	-- 改行コードは好みじゃないので表示しない
-	-- show_end_of_line = true,
-	-- vscode同様、現在のネスト範囲をハイライト
-	-- show_current_context = true,
-	-- show_current_context_start = true,
+	enabled = true,
+	indent = {
+		char = "▏",
+	},
 })
