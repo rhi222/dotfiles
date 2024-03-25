73af450ba7576baa61ee5c52d06d859c432773d6
rhi222 <ryouhei222@gmail.com>
2024-03-25 19:06:10 +0900

    clean: comment

--- .config/nvim/lua/config/fzf-lua.lua
+++ .config/nvim/lua/config/fzf-lua.lua
@@ -1,4 +1,5 @@
 -- https://github.com/ibhagwan/fzf-lua#usage
+-- https://github.com/ibhagwan/fzf-lua/blob/main/doc/fzf-lua.txt
 require("fzf-lua").setup({
 	grep = {
 		rg_opts = "--hidden --glob '!.git' --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e",
