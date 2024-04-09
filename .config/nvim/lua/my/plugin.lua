50c00307843a8d9e0a4a5a9203aa8ce9bd9bf618
rhi222 <ryouhei222@gmail.com>
2024-04-09 19:50:26 +0900

    add: neogit

--- .config/nvim/lua/my/plugin.lua
+++ .config/nvim/lua/my/plugin.lua
@@ -248,6 +248,20 @@ return {
 			"RainbowMultiDelim",
 		},
 	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = function()
+			require("my/plugins/neogit")
+		end,
+	},
 }
 
 -- 気になっているmodule
