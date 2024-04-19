526d6cf1d0fd9ceb5d90ea726539514a2855dc27
rhi222 <ryouhei222@gmail.com>
2024-04-19 12:45:44 +0900

    add: 設定追加

--- .config/nvim/lua/my/plugins/vscode.lua
+++ .config/nvim/lua/my/plugins/vscode.lua
@@ -5,6 +5,9 @@ vim.o.background = 'dark'
 
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
+    -- Enable transparent background
+    -- transparent = true,
+
     -- transparent = true,
     italic_comments = true,
     -- Underline `@markup.link.*` variants
