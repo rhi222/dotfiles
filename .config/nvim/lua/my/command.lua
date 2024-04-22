ad5aa5c388c4f20b6701380a3e1f74595ab02e9d
rhi222 <ryouhei222@gmail.com>
2024-04-22 11:39:16 +0900

    clean: リファクタリング

--- .config/nvim/lua/my/command.lua
+++ .config/nvim/lua/my/command.lua
@@ -1,74 +1,101 @@
-
 -- -------------------- user command {{{
 -- https://github.com/willelz/nvim-lua-guide-ja/blob/master/README.ja.md#%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%82%92%E5%AE%9A%E7%BE%A9%E3%81%99%E3%82%8B
-function OpenGitURL(mode)
-	local repo_name = vim.fn.systemlist(
-		"git config --get remote.origin.url | grep -oP '(?<=git@|http://)(.*)(?=.git)' | sed 's/:/\\//'"
+-- TODO: コマンドが複数になる場合、コマンド毎にファイル分割
+
+-- Retrieves the URL of the Git repository from the remote origin configuration.
+local function getRepositoryURL()
+	local repo_url = vim.fn.systemlist(
+		"git config --get remote.origin.url | grep -oP '(?<=git@|https://)(.*)(?=.git)' | sed 's/:/\\//'"
 	)[1]
-	--repo_nameにgithubの文字列が入るか判定
-	local is_github = string.find(repo_name, "github")
-	local is_gitlab = string.find(repo_name, "gitlab")
-	local is_bitbucket = string.find(repo_name, "bitbucket")
-	-- 明示的に対応したレポジトリ管理ツール以外はエラーを出力
-	if is_github == nil and is_gitlab == nil and is_bitbucket == nil then
-		print("This repository is neither github nor gitlab nor bitbucket")
-		return
+	if repo_url == "" then
+		print("Unable to retrieve repository URL")
+		return nil
 	end
-	local filepath = GetFilePathFromRepoRoot()
-	-- local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-	local hash = vim.fn.systemlist("git rev-parse HEAD")[1]
-	local start_line, end_line = GetCurrentLine(mode)
-	local url = GenerateGitUrl(repo_name, hash, filepath, start_line, end_line, is_gitlab, is_bitbucket)
-	print("Open: " .. url)
-	-- wsl-open
-	-- https://github.com/4U6U57/wsl-open/tree/master
-	vim.fn.jobstart("wsl-open " .. url)
+	return repo_url
 end
 
--- レポジトリrootからの相対パスを取得
-function GetFilePathFromRepoRoot()
+-- Determines the type of repository based on the URL.
+local function getRepositoryType(repo_url)
+	if string.find(repo_url, "github") then
+		return "github"
+	elseif string.find(repo_url, "gitlab") then
+		return "gitlab"
+	elseif string.find(repo_url, "bitbucket") then
+		return "bitbucket"
+	else
+		return nil
+	end
+end
+
+-- Gets the file path from the repository root.
+local function getFilePathFromRepoRoot()
 	local filename = vim.fn.expand("%")
-	local filepath_from_root = vim.fn.systemlist("git ls-files --full-name " .. filename)[1]
-	return filepath_from_root
+	local filepath = vim.fn.systemlist("git ls-files --full-name " .. filename)[1]
+	if filepath == "" then
+		print("Unable to find file in repository")
+		return nil
+	end
+	return filepath
 end
 
 -- normalモードの場合はカーソル位置の行数を取得
 -- visualモードの場合は選択範囲の行数を取得
-function GetCurrentLine(mode)
-	local start_line = 0
-	local end_line = 0
+local function getCurrentLineRange(mode)
 	if mode == "n" then
 		-- normalモードの場合はカーソル位置の行数を取得
-		start_line = vim.fn.line(".")
-		end_line = vim.fn.line(".")
+		local line = vim.fn.line(".")
+		return line, line
 	elseif mode == "v" then
-		start_line = vim.fn.getpos("'<")[2]
-		end_line = vim.fn.getpos("'>")[2]
-	else
-		-- do nothing
+		return vim.fn.getpos("'<")[2], vim.fn.getpos("'>")[2]
 	end
-	return start_line, end_line
+	return nil, nil
 end
 
-function GenerateGitUrl(repo_name, hash, filepath_from_root, start_line, end_line, is_gitlab, is_bitbucket)
-	-- gitlabの場合はhttp、fdevがhttps対応してないため
-	local url = (is_gitlab and "http://" or "https://")
-		.. repo_name
-		.. (is_bitbucket and "/src/" or "/blob/")
-		.. hash
-		.. "/"
-		.. filepath_from_root
-		.. (is_bitbucket and "#lines-" or "#L")
-		.. start_line
-		-- NOTE: gitlabとgithubで範囲選択の仕方が違うので注意
-		-- is_gitlabは - のみ
-		-- is_githubは -L となる
-		.. (
-			-- gitlabのときは -, githubのときは -L, bitbucketのときは :
-			is_bitbucket and ":" or (is_gitlab and "-" or "-L")
-		)
-		.. end_line
-	return url
+-- Generates a URL to the file in the Git hosting service.
+local function generateGitUrl(repo_type, repo_url, hash, filepath, start_line, end_line)
+	local base_url = (repo_type == "gitlab" and "http://" or "https://") .. repo_url
+	local path = (repo_type == "bitbucket" and "/src/" or "/blob/") .. hash .. "/" .. filepath
+	local line_ref = (repo_type == "bitbucket" and "#lines-" or "#L") .. start_line
+	-- NOTE: repo_typeで範囲選択の仕方が違うので注意
+	-- -- gitlabのときは -, githubのときは -L, bitbucketのときは :
+	if start_line ~= end_line then
+		line_ref = line_ref .. (repo_type == "gitlab" and "-" or (repo_type == "github" and "-L" or ":")) .. end_line
+	end
+	return base_url .. path .. line_ref
+end
+
+-- Main function to handle the open URL command.
+function OpenGitURL(mode)
+	local repo_url = getRepositoryURL()
+	if not repo_url then
+		return
+	end
+
+	local repo_type = getRepositoryType(repo_url)
+	-- 明示的に対応したレポジトリ管理ツール以外はエラーを出力
+	if not repo_type then
+		print("This repository is not supported. only github, gitlab, bitbucket are supported.")
+		return
+	end
+
+	local filepath = getFilePathFromRepoRoot()
+	if not filepath then
+		return
+	end
+
+	local hash = vim.fn.systemlist("git rev-parse HEAD")[1]
+	if hash == "" then
+		print("Unable to retrieve Git hash")
+		return
+	end
+
+	local start_line, end_line = getCurrentLineRange(mode)
+	local url = generateGitUrl(repo_type, repo_url, hash, filepath, start_line, end_line)
+
+	print("Opening URL: " .. url)
+	-- wsl-open
+	-- https://github.com/4U6U57/wsl-open/tree/master
+	vim.fn.jobstart("wsl-open " .. url)
 end
 
 vim.api.nvim_create_user_command("OpenGit", OpenGitURL, { nargs = 0 })
