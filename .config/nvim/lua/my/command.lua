066cba285afd2be0c5732bb9ef355fabdde32e69
rhi222 <ryouhei222@gmail.com>
2024-04-30 02:16:38 +0900

    add: 一次作業コマンド追加

--- .config/nvim/lua/my/command.lua
+++ .config/nvim/lua/my/command.lua
@@ -2,6 +2,7 @@
 -- https://github.com/willelz/nvim-lua-guide-ja/blob/master/README.ja.md#%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%82%92%E5%AE%9A%E7%BE%A9%E3%81%99%E3%82%8B
 
 return {
-	require("my/commands/open-git")
+	require("my/commands/open-git"),
+	require("my/commands/temporary-work"),
 }
 -- }}} -------------------------------
