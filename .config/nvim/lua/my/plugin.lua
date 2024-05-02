3cba99c6c1f2546f0731b8d25f9087ac1da4aff5
rhi222 <ryouhei222@gmail.com>
2024-05-02 09:49:07 +0900

    add: copilot chatと遅延読み込み

--- .config/nvim/lua/my/plugin.lua
+++ .config/nvim/lua/my/plugin.lua
@@ -58,9 +58,29 @@ return {
 	-- },
 	{
 		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
 		config = function()
 			require("my/plugins/copilot")
 		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		cmd = {
+			"CopilotChat",
+			"CopilotChatOpen",
+			"CopilotChatClose",
+			"CopilotChatToggle",
+		},
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
 	},
 	{
 		"zbirenbaum/copilot-cmp",
@@ -260,6 +280,9 @@ return {
 	},
 	{
 		"NeogitOrg/neogit",
+		cmd = {
+			"Neogit",
+		},
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- required
 			"sindrets/diffview.nvim", -- optional - Diff integration
