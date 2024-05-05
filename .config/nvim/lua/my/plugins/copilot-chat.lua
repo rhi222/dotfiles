58ad2ec033d214b521246d51743050aa2ffb21cc
rhi222 <ryouhei222@gmail.com>
2024-05-05 22:02:14 +0900

    add: copilot-chatの現在のバッファからのchat開始

--- .config/nvim/lua/my/plugins/copilot-chat.lua
+++ .config/nvim/lua/my/plugins/copilot-chat.lua
@@ -1,5 +1,6 @@
 -- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#configuration
-require("CopilotChat").setup({
+local CopilotChat = require("CopilotChat")
+CopilotChat.setup({
 	debug = true,
 })
 
@@ -7,3 +8,16 @@ require("CopilotChat").setup({
 vim.keymap.set({ "n" }, "<C-a>", function()
 	vim.cmd("CopilotChatOpen")
 end)
+
+local CopilotChatSelect = require("CopilotChat.select")
+
+-- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#tips
+-- 現在のバッファを読み込んでCopilotChatに質問を投げる関数
+_G.askCopilotWithBuffer = function()
+	local input = vim.fn.input("Quick Chat: ")
+	if input ~= "" then
+		CopilotChat.ask(input, { selection = CopilotChatSelect.buffer })
+	end
+end
+
+vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>lua _G.askCopilotWithBuffer()<CR>", { noremap = true, silent = true })
