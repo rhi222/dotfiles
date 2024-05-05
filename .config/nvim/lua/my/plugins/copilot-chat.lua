9f7d05e7c711b47d9e8f4adad173ffeb643887fa
rhi222 <ryouhei222@gmail.com>
2024-05-05 23:08:32 +0900

    clean: リファクタ

--- .config/nvim/lua/my/plugins/copilot-chat.lua
+++ .config/nvim/lua/my/plugins/copilot-chat.lua
@@ -1,19 +1,14 @@
 -- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#configuration
 -- Group all requires at the top
 local CopilotChat = require("CopilotChat")
+local CopilotActions = require("CopilotChat.actions")
 local CopilotChatSelect = require("CopilotChat.select")
-local copilotActions = require("CopilotChat.actions")
 local telescope = require("CopilotChat.integrations.telescope")
 
 CopilotChat.setup({
 	debug = true,
 	prompts = {},
 })
-
--- CopilotChatOpenのkeymapを設定
-vim.keymap.set({ "n" }, "<C-a>", function()
-	vim.cmd("CopilotChatOpen")
-end)
 
 -- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#tips
 -- 現在のバッファを読み込んでCopilotChatに質問を投げる関数
@@ -23,23 +18,20 @@ _G.askCopilotWithBuffer = function()
 		CopilotChat.ask(input, { selection = CopilotChatSelect.buffer })
 	end
 end
-
-vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>lua _G.askCopilotWithBuffer()<CR>", { noremap = true, silent = true })
-
--- CopilotChat.actionsをrequire
-local actions = require("CopilotChat.actions")
-
 -- CopilotChat - Help actionsの関数
 _G.showHelpActionsWithTelescope = function()
-	telescope.pick(actions.help_actions())
+	telescope.pick(CopilotActions.help_actions())
 end
-
 -- CopilotChat - Prompt actionsの関数
 _G.showPromptActionsWithTelescope = function()
-	telescope.pick(actions.prompt_actions())
+	telescope.pick(CopilotActions.prompt_actions())
 end
 
--- 新しい関数をキーマップに追加
+-- CopilotChatOpenのkeymapを設定
+vim.keymap.set({ "n" }, "<C-a>", function()
+	vim.cmd("CopilotChatOpen")
+end)
+vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>lua _G.askCopilotWithBuffer()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap(
 	"n",
 	"<C-d>",
