65c26f8eabfa92e58bfc4652fbb79cd08c7a0401
rhi222 <ryouhei222@gmail.com>
2024-05-06 13:25:25 +0900

    feat(nvim-cmp): streamline configuration

--- .config/nvim/lua/my/plugins/nvim-cmp.lua
+++ .config/nvim/lua/my/plugins/nvim-cmp.lua
@@ -27,13 +27,6 @@ cmp.setup({
 	}, {
 		{ name = "buffer" },
 	}),
-	-- formatting = {
-	--   format = lspkind.cmp_format({
-	--     mode = 'symbol', -- show only symbol annotations
-	--     maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-	--     ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-	--   })
-	-- }
 })
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
@@ -55,9 +48,5 @@ cmp.setup.cmdline({ "/", "?" }, {
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
 	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
+	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
 })
