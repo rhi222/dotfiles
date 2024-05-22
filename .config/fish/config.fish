43996f73b34cadba5d16e64d19ceb6b32c7b43cd
rhi222 <ryouhei222@gmail.com>
2024-05-22 16:20:11 +0900

    feat: Update fish and git configurations
    - Reorder abbreviations in fish config for better organization.
    - Add lazygit abbreviation in fish config.
    - Set git status to show untracked files as normal.
    - Set default git branch to 'main' in git config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,8 +26,8 @@ alias gu 'gitui'
 # コマンドにコメントをつけておく、fzfでのgrepability上がるため
 # https://qiita.com/wataash/items/ab0a8b86b60e782f537f#%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%81%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%88%E3%82%92%E3%81%A4%E3%81%91%E3%81%A6%E3%81%8A%E3%81%8F
 abbr --add gbr "git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))' # show recently touched branch"
-abbr --add dc docker compose
 abbr --add ggr 'cd (git rev-parse --show-toplevel)'
+abbr --add dc docker compose
 abbr --add dcl 'docker compose -f (find_docker_compose) logs -f --tail=500 # show current repository docker compose log'
 abbr --add dcd 'docker compose -f (find_docker_compose) down'
 abbr --add dcu 'docker compose -f (find_docker_compose) up --build'
@@ -40,6 +40,7 @@ function find_docker_compose
 end
 abbr --add cpe 'COMPOSE_PROFILES='
 abbr --add ld lazydocker
+abbr --add lg lazygit
 # ------------- }}}
 
 # ------------- font color setting {{{
