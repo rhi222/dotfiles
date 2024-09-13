2505a2beb8912849f3b6240d761deee1bddfb420
rhi222 <ryouhei222@gmail.com>
2024-09-13 11:47:28 +0900

    fix: rest.nvimのjsonformat

--- .config/nvim/lua/my/plugins/_rest-nvim.lua
+++ .config/nvim/lua/my/plugins/_rest-nvim.lua
@@ -1,6 +1,6 @@
 -- No .setup() call is needed! Just set your options via vim.g.rest_nvi
+---@type rest.Opts
 vim.g.rest_nvim = {
-
     -- https://github.com/rest-nvim/rest.nvim?tab=readme-ov-file#default-configuration
 }
 vim.keymap.set("n", "<C-e>", "<cmd>Rest run<CR>", { desc = "Run rest command" })
