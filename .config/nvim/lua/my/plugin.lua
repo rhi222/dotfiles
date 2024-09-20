610b3cf98018cf004e93df085187f1a47aa30964
rhi222 <ryouhei222@gmail.com>
2024-09-21 00:09:28 +0900

    add: neogit keymap

--- .config/nvim/lua/my/plugin.lua
+++ .config/nvim/lua/my/plugin.lua
@@ -278,14 +278,9 @@ return {
 	},
 	{
 		"NeogitOrg/neogit",
-		cmd = {
-			"Neogit",
-		},
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- required
 			"sindrets/diffview.nvim", -- optional - Diff integration
-
-			-- Only one of these is needed, not both.
 			"nvim-telescope/telescope.nvim", -- optional
 		},
 		config = function()
