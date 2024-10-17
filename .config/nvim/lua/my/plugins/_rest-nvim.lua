d4bc08f16d68375995d5d26123bc1cc5e06ab569
rhi222 <ryouhei222@gmail.com>
2024-10-17 22:33:24 +0900

    add: rest.nvimでssl証明書エラーはignore

--- .config/nvim/lua/my/plugins/_rest-nvim.lua
+++ .config/nvim/lua/my/plugins/_rest-nvim.lua
@@ -1,6 +1,10 @@
 -- No .setup() call is needed! Just set your options via vim.g.rest_nvi
 ---@type rest.Opts
 vim.g.rest_nvim = {
-    -- https://github.com/rest-nvim/rest.nvim?tab=readme-ov-file#default-configuration
+	-- https://github.com/rest-nvim/rest.nvim?tab=readme-ov-file#default-configuration
+	request = {
+		-- 予約記録のdev環境など検証系のエラー回避
+		skip_ssl_verification = true,
+	},
 }
 vim.keymap.set("n", "<C-e>", "<cmd>Rest run<CR>", { desc = "Run rest command" })
