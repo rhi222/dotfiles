78c655c29c7eef7a1496a4ac1312e6ccba543707
rhi222 <ryouhei222@gmail.com>
2024-11-07 00:43:12 +0900

    add: レポジトリによってjsのformatterを切り替え

--- .config/nvim/lua/my/plugins/conform.lua
+++ .config/nvim/lua/my/plugins/conform.lua
@@ -1,20 +1,43 @@
 -- https://github.com/stevearc/conform.nvim
 -- Conform will run multiple formatters sequentially, run the first available formatter
+-- NOTE: 開いているバッファにどのFormatterが割り当てられているか確認するコマンド:ConformInfo
+
 local formatter_js = { "biome", "prettier", stop_after_first = true }
+
+-- 現在のファイルがあるGitリポジトリのルートを取得する関数
+local function get_git_root()
+    local git_dir = vim.fn.finddir(".git", vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":h") .. ";")
+    if git_dir == "" then
+        return nil  -- `.git`ディレクトリが見つからない場合
+    else
+        return vim.fn.fnamemodify(git_dir, ":h")
+    end
+end
+
+-- Gitリポジトリのルートに`biome.json`があるかを確認し、フォーマッタを設定
+local function get_js_formatter()
+    local root_dir = get_git_root()
+    if root_dir and vim.fn.filereadable(root_dir .. "/biome.json") == 1 then
+        return { "biome" }  -- `biome.json`がある場合は`biome`を使う
+    else
+        return { "prettier" } -- `biome.json`がない場合は`prettier`を使う
+    end
+end
+
 require("conform").setup({
 	formatters_by_ft = {
 		bash = { "shfmt" },
 		html = { "prettier" },
-		javascript = formatter_js,
-		javascriptreact = formatter_js,
+		javascript = get_js_formatter,
+		javascriptreact = get_js_formatter,
 		json = { "prettier" },
 		json5 = { "prettier" },
 		lua = { "stylua" },
 		python = { "ruff", "black", stop_after_first = true },
 		rust = { "rustfmt", lsp_format = "fallback" },
 		sql = { "sqlfluff" },
-		typescript = formatter_js,
-		typescriptreact = formatter_js,
+		typescript = get_js_formatter,
+		typescriptreact = get_js_formatter,
 		xml = { "xmlformat" },
 		-- Use the "_" filetype to run formatters on filetypes that don't
 		-- have other formatters configured.
@@ -34,3 +57,4 @@ vim.api.nvim_create_user_command("Format", function(args)
 	end
 	require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
+
