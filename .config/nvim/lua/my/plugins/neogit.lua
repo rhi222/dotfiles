b02a2b8d29421ac0d4576cd9603299ba8cecfa08
rhi222 <ryouhei222@gmail.com>
2024-11-08 13:13:47 +0900

    add: neogit config
    行指定移動したいのでfalse

--- .config/nvim/lua/my/plugins/neogit.lua
+++ .config/nvim/lua/my/plugins/neogit.lua
@@ -1,3 +1,5 @@
--- init.lua
+-- https://github.com/NeogitOrg/neogit?tab=readme-ov-file#configuration
 local neogit = require("neogit")
-neogit.setup({})
+neogit.setup({
+	disable_line_numbers = false,
+})
