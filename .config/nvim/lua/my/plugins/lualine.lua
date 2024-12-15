b94a65f8f65dcbfd2c1fc57bbcbf6c6da7f3c60b
rhi222 <ryouhei222@gmail.com>
2024-12-15 10:00:47 +0900

    fix: alacrittyでlualine使うことによる表示崩れ

--- .config/nvim/lua/my/plugins/lualine.lua
+++ .config/nvim/lua/my/plugins/lualine.lua
@@ -1,9 +1,15 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 require("lualine").setup({
+	options = {
+		-- NOTE: trueだとalacrittyで表示崩れする
+		-- lualine_bにdiagnostics設定時 -> error, warnなど複数種類あるとき
+		-- lualine_xにfiletype設定時 -> txtファイル開いたとき
+		icons_enabled = false,
+	},
 	sections = {
+		lualine_b = { "branch", "diff" },
 		-- show fullpath
 		lualine_c = { { "filename", path = 2 } },
-		-- fileformatは非表示
 		lualine_x = { "filetype", "encoding" },
 	},
 })
