e5e963e66d086e1be2135d0b55c4b42d562b4791
rhi222 <ryouhei222@gmail.com>
2025-04-07 10:15:29 +0900

    clean: 変数名短く

--- .config/nvim/lua/my/plugins/copilot-chat.lua
+++ .config/nvim/lua/my/plugins/copilot-chat.lua
@@ -1,16 +1,16 @@
 -- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#configuration
 -- Group all requires at the top
-local CopilotChat = require("CopilotChat")
-local CopilotActions = require("CopilotChat.actions")
-local CopilotChatSelect = require("CopilotChat.select")
+local chat = require("CopilotChat")
+local actions = require("CopilotChat.actions")
+local select = require("CopilotChat.select")
 local telescope = require("CopilotChat.integrations.telescope")
 
-CopilotChat.setup({
+chat.setup({
 	debug = true,
 	prompts = {
 		ExplainBuffer = {
 			prompt = "/COPILOT_EXPLAIN Write an explanation for the selection as paragraphs of text.",
-			selection = CopilotChatSelect.buffer,
+			selection = select.buffer,
 		},
 	},
 	-- CopilotChatModels
@@ -24,21 +24,21 @@ CopilotChatFunctions = {}
 CopilotChatFunctions.askCopilotWithBuffer = function()
 	local input = vim.fn.input("Quick Chat with Buffer: ")
 	if input ~= "" then
-		CopilotChat.ask(input, { selection = CopilotChatSelect.buffer })
+		chat.ask(input, { selection = select.buffer })
 	end
 end
 CopilotChatFunctions.askCopilotWithVisual = function()
 	local input = vim.fn.input("Quick Chat with Visual: ")
 	if input ~= "" then
-		CopilotChat.ask(input, { selection = CopilotChatSelect.visual })
+		chat.ask(input, { selection = select.visual })
 	end
 end
 -- telescopeで選択肢を表示する関数
 CopilotChatFunctions.showHelpActionsWithTelescope = function()
-	telescope.pick(CopilotActions.help_actions())
+	telescope.pick(actions.help_actions())
 end
 CopilotChatFunctions.showPromptActionsWithTelescope = function()
-	telescope.pick(CopilotActions.prompt_actions())
+	telescope.pick(actions.prompt_actions())
 end
 
 -- CopilotChatOpenのkeymapを設定
