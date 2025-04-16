a7b4030d102fe3300fcbbaf8c919ca9d332e064a
rhi222 <ryouhei222@gmail.com>
2025-04-16 10:11:28 +0900

    feat: telescopeのfile findもkey map

--- .config/nvim/lua/my/plugin.lua
+++ .config/nvim/lua/my/plugin.lua
@@ -187,22 +187,6 @@ return {
 		end,
 	}, ]]
 	-- finder
-	-- https://github.com/danielfalk/smart-open.nvim
-	-- https://minerva.mamansoft.net/%F0%9F%93%98Articles/%F0%9F%93%98%E3%81%82%E3%81%BE%E3%82%8A%E7%B4%B9%E4%BB%8B%E3%81%95%E3%82%8C%E3%81%A6%E3%81%84%E3%81%AA%E3%81%84%E3%81%91%E3%81%A9+%E3%81%8B%E3%81%91%E3%81%8C%E3%81%88%E3%81%AE%E3%81%AA%E3%81%84Neovim%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%81%9F%E3%81%A1#smart-open.nvim
-	{
-		"danielfalk/smart-open.nvim",
-		branch = "0.2.x",
-		config = function()
-			require("telescope").load_extension("smart_open")
-		end,
-		dependencies = {
-			"kkharji/sqlite.lua",
-			-- Only required if using match_algorithm fzf
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
-			{ "nvim-telescope/telescope-fzy-native.nvim" },
-		},
-	},
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8", -- 公式READMEがtag指定推奨
@@ -240,6 +224,22 @@ return {
 			require("my/plugins/telescope")
 		end,
 	},
+	-- https://github.com/danielfalk/smart-open.nvim
+	-- https://minerva.mamansoft.net/%F0%9F%93%98Articles/%F0%9F%93%98%E3%81%82%E3%81%BE%E3%82%8A%E7%B4%B9%E4%BB%8B%E3%81%95%E3%82%8C%E3%81%A6%E3%81%84%E3%81%AA%E3%81%84%E3%81%91%E3%81%A9+%E3%81%8B%E3%81%91%E3%81%8C%E3%81%88%E3%81%AE%E3%81%AA%E3%81%84Neovim%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%81%9F%E3%81%A1#smart-open.nvim
+	{
+		"danielfalk/smart-open.nvim",
+		branch = "0.2.x",
+		config = function()
+			require("telescope").load_extension("smart_open")
+		end,
+		dependencies = {
+			"kkharji/sqlite.lua",
+			-- Only required if using match_algorithm fzf
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+			{ "nvim-telescope/telescope-fzy-native.nvim" },
+		},
+	},
 	{
 		"ibhagwan/fzf-lua",
 		-- optional for icon support
@@ -247,7 +247,7 @@ return {
 		keys = {
 			{ "<c-g>", "<cmd>lua require('fzf-lua').grep()<CR>", mode = "n", silent = true },
 			-- note: telescopeのsmart-openが賢そうなので移行してみる
-			-- { "<c-p>", "<cmd>lua require('fzf-lua').files()<CR>", mode = "n", silent = true },
+			{ "<c-[>", "<cmd>lua require('fzf-lua').files()<CR>", mode = "n", silent = true },
 		},
 		config = function()
 			require("my/plugins/fzf-lua")
