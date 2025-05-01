7d42f72671a69f12e4e908b73965436c65e47c1e
rhi222 <ryouhei222@gmail.com>
2025-05-01 18:30:33 +0900

    fix: デフォルトではdeprecatedなcanaryブランチを指していたので、明示的にmainブランチを指す

--- .config/nvim/lua/my/plugin.lua
+++ .config/nvim/lua/my/plugin.lua
@@ -83,6 +83,7 @@ return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		event = "VeryLazy",
+		branch = "main",
 		dependencies = {
 			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
