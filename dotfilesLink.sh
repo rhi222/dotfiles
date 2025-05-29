e5ba936681ef6207d657805293f40c0654c65842
rhi222 <ryouhei222@gmail.com>
2025-05-30 00:30:33 +0900

    feat: mise周りの変更対応

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,5 +5,7 @@ ln -snf $PWD/.tmux.conf ~/.tmux.conf
 ln -snf $PWD/.psqlrc ~/.psqlrc
 ln -snf $PWD/.config/fish/config.fish ~/.config/fish/config.fish
 ln -snf $PWD/.config/nvim ~/.config/nvim
+ln -snf $PWD/.config/git ~/.config/git
+ln -snf $PWD/.config/mise ~/.config/mise
 ln -snf $PWD/.config/gitui ~/.config/gitui
 ln -snf $PWD/.config/lazygit ~/.config/lazygit
