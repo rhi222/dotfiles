bc9c54b7cfcc91563cfa29926168a56688ff4a87
rhi222 <ryouhei222@gmail.com>
2025-07-18 11:06:59 +0900

    feat: symlink追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,9 @@ ln -snf $PWD/.gitconfig ~/.gitconfig
 ln -snf $PWD/.tmux.conf ~/.tmux.conf
 ln -snf $PWD/.psqlrc ~/.psqlrc
 ln -snf $PWD/.config/claude ~/.config/claude
+ln -snf $PWD/.config/claude/CLAUDE.md ~/.claude/CLAUDE.md
+ln -snf $PWD/.config/claude/settings.json ~/.claude/settings.json
+ln -snf $PWD/.config/claude/commands ~/.claude/commands
 ln -snf $PWD/.config/fish/config.fish ~/.config/fish/config.fish
 ln -snf $PWD/.config/nvim ~/.config/nvim
 ln -snf $PWD/.config/git ~/.config/git
