e519e68ca8697465ae61f60cc3dd72f09592bac3
rhi222 <ryouhei222@gmail.com>
2025-08-07 18:15:48 +0900

    feat: 設定ファイル分離

---
+++ .config/fish/my/conf.d/07-aliases.fish
@@ -0,0 +1,14 @@
+# Aliases
+
+# Git
+alias cdrr "cd (git rev-parse --show-toplevel)"
+alias gu gitui
+
+# Terminal & Tools
+alias tmux 'tmux -2' # open tmux in 256color
+alias vi nvim
+alias view 'nvim -R'
+alias rf 'source ~/.config/fish/config.fish' # reload fish config
+
+# SSH
+alias sshe 'cocot -t UTF-8 -p EUC-JP -- ssh' # EUC-JP端末へのアクセス
