b5bf2d4b34cbcfe26c1571d2470ea0cbd25516e2
rhi222 <ryouhei222@gmail.com>
2025-09-18 19:19:03 +0900

    feat: lualineの表示変更

--- .config/nvim/lua/my/plugins/lualine.lua
+++ .config/nvim/lua/my/plugins/lualine.lua
@@ -7,9 +7,15 @@ require("lualine").setup({
 		icons_enabled = false,
 	},
 	sections = {
-		lualine_b = { "branch", "diff" },
-		-- show fullpath
-		lualine_c = { { "filename", path = 2 } },
+		-- branchは非表示, filenameの表示スペースを確保したい
+		lualine_b = { "diff" },
+		-- pathの値で表示形式を変更可能
+		-- 0: Just the filename
+		-- 1: Relative path
+		-- 2: Absolute path
+		-- 3: Absolute path, with tilde as the home directory
+		-- 4: Filename and parent dir, with tilde as the home directory
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "filetype", "encoding" },
 	},
 })
