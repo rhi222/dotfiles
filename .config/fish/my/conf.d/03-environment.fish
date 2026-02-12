4284fb27ef5179e5c5c8aa6e61b833cca8a9fd8d
rhi222 <ryouhei222@gmail.com>
2026-02-12 23:40:50 +0900

    fix(fish): 03-environment.fishの環境変数に-gxフラグを追加

--- .config/fish/my/conf.d/03-environment.fish
+++ .config/fish/my/conf.d/03-environment.fish
@@ -1,13 +1,13 @@
 # Environment variables and tool settings
 
-set PSQL_EDITOR nvim
-set GIT_EDITOR 'nvim -u $HOME/.config/nvim/init.lua'
+set -gx PSQL_EDITOR nvim
+set -gx GIT_EDITOR 'nvim -u $HOME/.config/nvim/init.lua'
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-set TERM screen-256color
+set -gx TERM screen-256color
 
 # for copilot at with zscaler credential
-set NODE_EXTRA_CA_CERTS /usr/local/share/ca-certificates/zscaler.cer
+set -gx NODE_EXTRA_CA_CERTS /usr/local/share/ca-certificates/zscaler.cer
 
 # zoxide integration
 if type -q zoxide
