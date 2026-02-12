b4d7ba6cbfadf90e48b09c764a859a6562ffa5f6
rhi222 <ryouhei222@gmail.com>
2026-02-12 23:48:57 +0900

    refactor(fish): 05-colors.fishを03-environment.fishに統合

--- .config/fish/my/conf.d/03-environment.fish
+++ .config/fish/my/conf.d/03-environment.fish
@@ -6,6 +6,11 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx TERM screen-256color
 
+# Font color settings
+# https://fishshell.com/docs/current/cmds/set_color.html
+# https://reiichii.hateblo.jp/entry/2022/01/05/194823
+set -U black brblack # 背景色と同化して読めないため
+
 # for copilot at with zscaler credential
 set -gx NODE_EXTRA_CA_CERTS /usr/local/share/ca-certificates/zscaler.cer
 
