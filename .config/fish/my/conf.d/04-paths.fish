93978203c8c7a950995ba3e5912c883ffeaac297
rhi222 <ryouhei222@gmail.com>
2026-02-12 23:43:47 +0900

    chore(fish): 04-paths.fishから移行済みのVolta設定を削除

--- .config/fish/my/conf.d/04-paths.fish
+++ .config/fish/my/conf.d/04-paths.fish
@@ -17,14 +17,6 @@ fish_add_path $HOME/.local/bin
 # for fzf
 fish_add_path $HOME/.fzf/bin
 
-# volta setting
-# nodeのバージョン管理はmiseに移行しているためコメントアウト
-# https://docs.volta.sh/guide/getting-started
-# http://gitlab.fdev/webconnect/material/material_registration/-/merge_requests/6511
-# set VOLTA_FEATURE_PNPM 1
-# set -gx VOLTA_HOME "$HOME/.volta"
-# fish_add_path $VOLTA_HOME/bin
-
 # for win32yank
 # https://qiita.com/v2okimochi/items/f53edcf79a4b71f519b1#%E3%83%9E%E3%82%A6%E3%82%B9%E6%93%8D%E4%BD%9C%E3%82%84%E3%82%AF%E3%83%AA%E3%83%83%E3%83%97%E3%83%9C%E3%83%BC%E3%83%89%E5%85%B1%E6%9C%89%E3%82%92%E8%A8%AD%E5%AE%9A%E3%81%99%E3%82%8B
 fish_add_path $HOME/bin
