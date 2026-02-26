5c2a8d2bd3f90dd21f84ada93f151dd989747145
rhi222 <ryouhei222@gmail.com>
2026-02-26 16:16:23 +0900

    refactor(fish): wt/wtdのworktree選択処理を__wt_selectに共通化
    fzf選択・awk変換・previewを共通ヘルパーに抽出し、
    .wt配下かどうかを示す[.wt]/[main]タグ表示を追加

--- .config/fish/my/functions/wtd.fish
+++ .config/fish/my/functions/wtd.fish
@@ -1,17 +1,9 @@
 # git worktree削除（fzfで選択）
-# 表示順: branch path commit_hash
 function wtd
-    set selection (git wt | tail -n +2 | \
-        awk '{if ($1 == "*") print "* "$3" "$2" "$4; else print "  "$2" "$1" "$3}' | \
-        fzf --ansi --reverse --height=80% \
-        --prompt="delete worktree > " \
-        --preview 'echo {} | awk "{if (\$1 == \"*\") print \$2; else print \$1}" | xargs -I{} git log --oneline -10 {}')
+    set selection (__wt_select "delete worktree")
+    or return 0
 
-    if test -z "$selection"
-        return 0
-    end
-
-    set worktree_path (echo $selection | awk '{if ($1 == "*") print $3; else print $2}')
+    set worktree_path (echo $selection | awk '{if ($1 == "*") print $4; else print $3}')
     echo "削除: $worktree_path"
     git worktree remove "$worktree_path"
 end
