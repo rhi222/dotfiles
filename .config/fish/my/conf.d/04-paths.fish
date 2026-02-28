832f1fd22d03857110e3acdf63f3f25ca3b3c7df
rhi222 <ryouhei222@gmail.com>
2026-02-28 12:31:46 +0900

    chore(mise): ツールをmise管理に統一し手動PATH設定を削除
    deno, fzf, gh, bat, eza, zoxideをmise config.tomlに追加し、
    04-paths.fishからdeno, fzf, bunの手動PATH設定を削除。
    並列インストール用にjobs=4を設定。

--- .config/fish/my/conf.d/04-paths.fish
+++ .config/fish/my/conf.d/04-paths.fish
@@ -7,15 +7,8 @@
 set -gx GOPATH $HOME/go
 fish_add_path $GOPATH/bin
 
-# for deno
-set -gx DENO_INSTALL $HOME/.deno
-fish_add_path $DENO_INSTALL/bin
-
 # for pip3
 fish_add_path $HOME/.local/bin
-
-# for fzf
-fish_add_path $HOME/.fzf/bin
 
 # for win32yank
 # https://qiita.com/v2okimochi/items/f53edcf79a4b71f519b1#%E3%83%9E%E3%82%A6%E3%82%B9%E6%93%8D%E4%BD%9C%E3%82%84%E3%82%AF%E3%83%AA%E3%83%83%E3%83%97%E3%83%9C%E3%83%BC%E3%83%89%E5%85%B1%E6%9C%89%E3%82%92%E8%A8%AD%E5%AE%9A%E3%81%99%E3%82%8B
@@ -27,10 +20,6 @@ fish_add_path /mnt/c/Program\ Files/Docker/Docker/resources/bin
 
 # rust setting
 fish_add_path $HOME/.cargo/bin
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-fish_add_path $BUN_INSTALL/bin
 
 # Rye
 # https://github.com/astral-sh/rye
