24a08bf2a88e0f0b252da5f8fb6bfc831a29c308
rhi222 <ryouhei222@gmail.com>
2026-02-28 12:38:54 +0900

    feat(yazi): yazi本体とyazi.nvimの設定を強化
    yazi.tomlにソート・レイアウト・プレビュー設定を追加。
    yazi.nvimにyaイベント検知、バッファハイライト、fzf-lua grep連携を追加。

--- .config/nvim/lua/my/plugins/finder/yazi.lua
+++ .config/nvim/lua/my/plugins/finder/yazi.lua
@@ -2,4 +2,14 @@ require("yazi").setup({
 	open_for_directories = true,
 	floating_window_scaling_factor = 0.6,
 	yazi_floating_window_border = "rounded",
+	use_ya_as_event_reader = true,
+	highlight_hovered_buffers_in_same_directory = true,
+	integrations = {
+		grep_in_directory = function(directory)
+			require("fzf-lua").live_grep({ cwd = directory })
+		end,
+		grep_in_selected_files = function(selected_files)
+			require("fzf-lua").live_grep({ search_paths = selected_files })
+		end,
+	},
 })
