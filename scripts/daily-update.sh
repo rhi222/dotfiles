de46eb1efee08e344761d16822adebe0bad65313
rhi222 <ryouhei222@gmail.com>
2026-03-04 09:16:44 +0900

    feat: 個人スクリプトをdotfilesで管理
    ~/my-scripts/ を scripts/ としてリポジトリに取り込み、
    dotfilesLink.sh で ~/scripts へのシンボリックリンクを設定

---
+++ scripts/daily-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+cargo install-update -a
+mise self-update -y
+mise upgrade
+nvim --headless "+Lazy! update" +qa
+nvim --headless -c 'autocmd User MasonUpdateAllComplete quitall' -c 'MasonUpdateAll'
