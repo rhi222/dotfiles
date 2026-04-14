6d0ee82a0bd0c9156fc976e68da4d96ccd82e7f9
rhi222 <ryouhei222@gmail.com>
2026-04-14 15:06:42 +0900

    fix(fish): fzf.fishプラグイン移行でCtrl+Tファイル検索が効かなくなった問題を修正
    FZF_CTRL_R_OPTSからfzf_history_time_formatへ移行し、
    fzf_configure_bindingsでCtrl+Tにディレクトリ検索を再バインド

--- .config/fish/my/conf.d/10-fzf.fish
+++ .config/fish/my/conf.d/10-fzf.fish
@@ -1,5 +1,10 @@
 # fzf settings
-
-# Ctrl+R 履歴検索のカスタマイズ
-# --with-nth=2.. : エポックタイムスタンプ列を非表示にし、コマンドのみ表示
-set -gx FZF_CTRL_R_OPTS '--with-nth=2..'
+#
+# キーバインド: PatrickF1/fzf.fish のデフォルトを上書き
+#   Ctrl+T → ディレクトリ/ファイル検索（デフォルトは Alt+Ctrl+F）
+fzf_configure_bindings --directory=\ct
+#
+# Ctrl+R の履歴検索は fzf.fish プラグイン（PatrickF1/fzf.fish）の
+# _fzf_search_history が担当する。タイムスタンプ列を非表示にするため
+# fzf_history_time_format を空文字で上書きする（区切り棒 │ は残る）。
+set -gx fzf_history_time_format ''
