09116af37b7fe6a38123b499c36bc4930563fe8e
rhi222 <ryouhei222@gmail.com>
2026-07-10 09:55:38 +0900

    fix(nvim): yazi終了時にVimResizedがdead channelへjobresizeするE900を回避
    yazi.nvim 13.1.5のterminal job終了とbuffer wipeの間のraceが原因。
    TermCloseで当該bufferのVimResized autocmdを即時解除して抑止する。

--- .config/nvim/lua/my/plugins/finder/yazi.lua
+++ .config/nvim/lua/my/plugins/finder/yazi.lua
@@ -5,6 +5,25 @@ require("yazi").setup({
 	use_ya_as_event_reader = true,
 	highlight_hovered_buffers_in_same_directory = true,
 	clipboard_register = "+",
+	hooks = {
+		-- yazi.nvim 13.1.5 では terminal job 終了から buffer wipe 完了までの間に
+		-- VimResized autocmd が dead channel に jobresize して E900 を投げる race
+		-- がある。TermClose で当該 buffer の VimResized autocmd を即時剥がして潰す。
+		yazi_opened = function(_, content_buffer, _)
+			vim.api.nvim_create_autocmd("TermClose", {
+				buffer = content_buffer,
+				once = true,
+				callback = function()
+					if vim.api.nvim_buf_is_valid(content_buffer) then
+						vim.api.nvim_clear_autocmds({
+							event = "VimResized",
+							buffer = content_buffer,
+						})
+					end
+				end,
+			})
+		end,
+	},
 	integrations = {
 		grep_in_directory = function(directory)
 			require("fzf-lua").live_grep({ cwd = directory })
