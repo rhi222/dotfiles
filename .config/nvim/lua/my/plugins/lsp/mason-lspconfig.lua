a5975bc4f25e348dcb0f606668cd48927d384f34
rhi222 <ryouhei222@gmail.com>
2026-08-22 20:42:02 +0900

    chore: migrate Python LSP to ty

--- .config/nvim/lua/my/plugins/lsp/mason-lspconfig.lua
+++ .config/nvim/lua/my/plugins/lsp/mason-lspconfig.lua
@@ -12,17 +12,17 @@ require("mason-lspconfig").setup({
 		"lua_ls",
 		"markdown_oxide",
 		"prismals",
-		"pylsp",
 		"ruff",
 		"sqlls",
 		"tailwindcss",
 		"ts_ls",
+		"ty",
 		"yamlls",
 	},
 	-- automatic_enable は全インストール済みserverが対象。ensure_installedから
 	-- 外しただけでは既存のMarksmanも起動するため、移行元を明示的に除外する。
 	automatic_enable = {
-		exclude = { "marksman" },
+		exclude = { "marksman", "pylsp" },
 	},
 	-- サーバー固有設定は ~/.config/nvim/lsp/*.lua で定義。
 })
