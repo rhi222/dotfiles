6087ccd11712861c6b003b21a240cf1d0e1920e9
rhi222 <ryouhei222@gmail.com>
2026-09-08 12:19:21 +0900

    refactor(nvim): blink capabilityの失敗を隠さない

--- .config/nvim/lua/my/plugins/lsp/utils.lua
+++ .config/nvim/lua/my/plugins/lsp/utils.lua
@@ -2,10 +2,7 @@ local M = {}
 
 function M.get_capabilities()
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	local ok, blink = pcall(require, "blink.cmp")
-	if ok then
-		capabilities = blink.get_lsp_capabilities(capabilities)
-	end
+	capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 	capabilities.textDocument.foldingRange = {
 		dynamicRegistration = false,
 		lineFoldingOnly = true,
